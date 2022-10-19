fd1f489fd66d0f1863d3b273fb406e8c817518f3
Godi Blasio <100287431+BlasioGodi@users.noreply.github.com>
2022-10-19 17:46:35 +0300

    Revit API Example
    Revit API Example file initialization

---
+++ TypeParameterUpdater/Inception_Child/Source/Inception_Child/ThisDocument.Designer.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 10/19/2022
+ * Time: 17:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Inception_Child {
+
+    public sealed partial class ThisDocument : Autodesk.Revit.UI.Macros.DocumentEntryPoint {
+        
+        public event System.EventHandler Startup;
+        
+        public event System.EventHandler Shutdown;
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        private void OnStartup() {
+        }
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override void FinishInitialization() {
+            base.FinishInitialization();
+            this.OnStartup();
+            this.InternalStartup();
+            if ((this.Startup != null)) {
+                this.Startup(this, System.EventArgs.Empty);
+            }
+        }
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override void OnShutdown() {
+            if ((this.Shutdown != null)) {
+                this.Shutdown(this, System.EventArgs.Empty);
+            }
+            base.OnShutdown();
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override string PrimaryCookie {
+            get {
+                return "ThisDocument";
+            }
+        }
+    }
+}
---
+++ TypeParameterUpdater/Inception_Child/Source/Inception_Child/ThisDocument.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 10/19/2022
+ * Time: 17:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inception_Child
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("4E1CDED4-BF3C-4BB8-B0B9-5B29866015AF")]
+	public partial class ThisDocument
+	{
+		private void Module_Startup(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Module_Shutdown(object sender, EventArgs e)
+		{
+
+		}
+
+		#region Revit Macros generated code
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(Module_Startup);
+			this.Shutdown += new System.EventHandler(Module_Shutdown);
+		}
+		#endregion
+	}
+}
