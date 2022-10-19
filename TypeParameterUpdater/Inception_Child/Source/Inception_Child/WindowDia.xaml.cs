1268315f81a7b8942557e3535bab3b43e455e0c2
Godi Blasio <100287431+BlasioGodi@users.noreply.github.com>
2022-10-19 23:29:38 +0300

    Updated Files

---
+++ TypeParameterUpdater/Inception_Child/Source/Inception_Child/WindowDia.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * ${res:XML.StandardHeader.CreatedByBlasio}
+ * User: ${BlasioGodi}
+ * Date: 10/19/2022
+ * Time: 21:11
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Inception_Child
+{
+	/// <summary>
+	/// Interaction logic for WindowDia.xaml
+	/// </summary>
+	public partial class WindowDia : Window
+	{
+		public WindowDia()
+		{
+			InitializeComponent();
+		}
+	}
+}
